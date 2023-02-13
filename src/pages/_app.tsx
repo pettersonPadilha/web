ac0427d5d93e7a08e8eb5e0483ea6f2ab33b6e72
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-13 18:44:05 -0400

    Adicionando styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,12 @@
 import {AppProps} from "next/app"
+import { GlobalStyle } from "../styles/global";
+
+
 export default function App({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Component {...pageProps} />
+    <GlobalStyle/>
+    </>
+  );
 }
