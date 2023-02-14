992a11bbe142170090f5a36894160f3f7f5cd6c5
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-13 23:18:34 -0400

    Estilo dos input components

---
+++ src/Components/ui/Input/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    .input {
+        margin-bottom: 1rem;
+        height: 40px;
+        border: 0;
+        border-radius: 0.5rem;
+        background-color: var(--gray-900);
+        color: white;
+        padding: 1rem;
+        border: 1px solid var(--gray-100);
+    }
+    .input::placeholder{
+        color: rgba(255,255,255,0.8);
+    }
+
+
+`
