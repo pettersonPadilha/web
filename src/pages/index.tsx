e63e25a7f92dd3052cd83bd032b89a45efa431d2
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-13 23:18:08 -0400

    Criando input de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import LogoImg from "../../public/logo/logo.png";
+import { Container,Content } from "../styles/global";
+import {Input} from "../Components/ui/Input/index";
 export default function Home() {
   return (
     <>
-    <h1>pops pizza</h1>
+        <Head> <title>Pops Pizza | Home</title>  </Head>
+        <Container>
+            <Image src={LogoImg} alt="logo pops Pizza"/>
+            <Content>
+                <form>
+                    <Input placeholder="Digite seu e-mail" type="text"/>
+                    <Input placeholder="Digite sua senha" type="password"/>
+                </form>
+            </Content>
+        </Container> 
     </>
   )
 }
