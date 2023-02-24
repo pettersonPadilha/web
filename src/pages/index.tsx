18b96fdfc7db8a11b913a24cca8fabe93ceb0abc
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-24 15:36:05 -0400

    Criando os components de input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-
+import Head  from "next/head";
+import styles from "../styles/Home.module.scss";
+import Image from "next/image"
+import logoImg from "../../public/logo/logo.svg";
+import Input from "../components/ui/Input";
 export default function Home() {
   return (
     <>   
-       <div>
-          <h1>Olá mundo !</h1>
+       <Head> <title>Pops Pizza</title> </Head>
+       <div className={styles.containerCenter}>
+          <Image src={logoImg} alt="logo"/>
+          <div className={styles.login}>
+              <form action="">
+                <Input placeholder="Digite seu email" type="text"/>
+                <Input placeholder="Digite sua senha" type="password"/>
+              </form>
+          </div>
        </div>
     </>
   )
