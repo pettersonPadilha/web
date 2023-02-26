8b8d239b2c755cb4d3e3148445bab2ada34d300b
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-25 23:14:14 -0400

    Protegendo rotas privadas melhorando

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from "react";
+import {createContext, ReactNode, useState,useEffect} from "react";
 import {api} from "../services/apiClient"
 import {destroyCookie, setCookie, parseCookies} from "nookies";
 import Router from "next/router";
@@ -47,6 +47,21 @@ export function signOut() {
 export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+       const {"@nextauth.token":token} = parseCookies();
+
+       if(token) {
+            api.get("/me").then(response => {
+                const {id, name, email} = response.data;
+
+                setUser({id, name, email});
+            })
+            .catch(() => {
+                signOut();
+            })
+       }
+    } ,[])
 
     {/* user login function*/}
    async  function signIn({email, password}:SignInProps) {
