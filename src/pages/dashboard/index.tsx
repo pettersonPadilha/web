696a1cafea20d790e1d20748ffaa5faecd5c2e01
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-25 22:58:55 -0400

    Controlando as rotas privadas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,13 @@
+import { canSSRAuth } from "../../utils/canSSRAuth";
+
 export default function Dashboard() {
     return(
         <h1>Bem vindo ao dashboard</h1>
     );
 }
+
+export const getServerSideProps = canSSRAuth(async (context) => {
+    return {
+        props: {}
+    }
+}) 
